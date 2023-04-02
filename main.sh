83692b2438bb1ea5f60d827acf053813cfce25b3
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-04-03 01:30:22 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -112,6 +112,8 @@ function dir_xray() {
     print_install "Membuat direktori xray"
     mkdir -p /etc/{xray,vmess,websocket,vless,trojan,shadowsocks}
     mkdir -p /usr/sbin/xray/
+    mkdir -p /etc/xray
+    mkdir -p /usr/local/etc/xray
     mkdir -p /var/log/xray/
     mkdir -p /var/www/html/
     mkdir -p /etc/rizkihdyt/
@@ -180,7 +182,7 @@ function install_xray(){
     curl -s ipinfo.io/city >> /etc/xray/city
     curl -s ipinfo.io/org | cut -d " " -f 2-10 >> /etc/xray/isp
     latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-    xraycore_link="xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
+    xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
     bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version $latest_version >/dev/null 2>&1
     cd `mktemp -d`
     curl -sL "$xraycore_link" -o xray.zip
